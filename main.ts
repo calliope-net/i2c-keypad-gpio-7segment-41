e9652b115379e7eb6cc8f67cf1e96ac309d1b45b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-11-17 14:51:32 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,25 @@ function toByte (ziffer: number) {
     if (ziffer == 0) {
         return qwiicgpio.parseint("0111111", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 1) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("0000110", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 2) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1011011", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 3) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1001111", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 4) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1100110", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 5) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1101101", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 6) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1111101", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 7) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("0000111", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 8) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1111111", qwiicgpio.eRadix.BIN)
     } else if (ziffer == 9) {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1101111", qwiicgpio.eRadix.BIN)
     } else {
-        return qwiicgpio.parseint("", qwiicgpio.eRadix.BIN)
+        return qwiicgpio.parseint("1000000", qwiicgpio.eRadix.BIN)
     }
 }
 pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
